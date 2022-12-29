4a2f59bc2b6fa9ad0cc7caa323df3c9753e24f0b
p0nyqz <ponyquiz@gmail.com>
2022-12-30 00:24:22 +0100

    config: toggleterm keymapping to toggle terminal

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,3 +1,8 @@
-require("toggleterm").setup {}
+require("toggleterm").setup {
+    size = 20,
+    open_mapping = [[<leader>t]],
+    shade_filetypes = {},
+    direction = "horizontal",
+}
 
-vim.api.nvim_set_keymap("n", "<leader>t", ":ToggleTerm<CR>", {noremap = true, silent = true})
+-- vim.api.nvim_set_keymap("n", "<leader>t", ":ToggleTerm<CR>", {noremap = true, silent = true})

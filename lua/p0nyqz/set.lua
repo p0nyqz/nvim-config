736f51de4f10670c92ea6c09bc35f76d69ed4fd6
p0nyqz <ponyquiz@gmail.com>
2023-03-03 01:16:22 +0100

    fat cursor

--- lua/p0nyqz/set.lua
+++ lua/p0nyqz/set.lua
@@ -4,6 +4,7 @@ vim.opt.cursorline = true
 -- vim.cmd('highlight CursorLine guibg=#FF00FF')
 
 vim.o.guifont = 'JetBrainsMono Nerd Font:h16'
+vim.o.guicursor = vim.o.guicursor .. ",a:block"
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
